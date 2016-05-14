72e3afa4220036930bf653a9d67efc54c306c92c
Dylan Stark <dylan.stark@gmail.com>
2016-05-14 06:08:34 -0500

    Updates code().

--- acsmsrs/R/code.R
+++ acsmsrs/R/code.R
@@ -1,19 +1,12 @@
 #' Construct coded name
 #'
-#' @param strs character vector.
-#' @param check logical specifying whether to check the variable code
+#' @param ... character string components of a name.
 #' @return The coded variable name.
 #'
 #' @examples
-#' code(c("GF", "A20to24"))
+#' code("GF", "A20to24")
 #'
 #' @export
-code <- function(strs, check = TRUE) {
-    full_name <- paste0(strs, collapse = "_")
-    if (check && !(full_name %in% coded_data$code)) {
-        warning("Could not find matching variable for ", full_name)
-        full_name <- NA
-    }
-
-    full_name
+code <- function(...) {
+    paste(..., sep = "_")
 }
