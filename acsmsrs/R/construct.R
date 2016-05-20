f1d5681765cbed5a0e09efd5f8439c76907f194c
Dylan Stark <dylan.stark@gmail.com>
2016-05-19 19:37:23 -0500

    Finishes support for writing child poverty measure.

--- acsmsrs/R/construct.R
+++ acsmsrs/R/construct.R
@@ -7,12 +7,46 @@ geo_ids <- function(geo) {
            geo$tract)
 }
 
+unique_tables <- function(columns) {
+    unique(sapply(stringr::str_split(columns, "_"), function(x) x [[1]]))
+}
+
+survey <- function(end_year, span) {
+    get(paste("codes", end_year, span, sep = "_"))
+}
+
+# Given a list of variables and associate survey end year and span,
+# return a corresponding list of coded labels.
+coded_variables <- function(end_year, span, acs_colnames) {
+    tables <- unique_tables(acs_colnames)
+
+    # Combine all data frames for these tables
+    survey <- survey(2013, 1)
+    data <- Reduce(rbind, survey[tables])
+
+    # Match coded column names
+    matches <- merge(data.frame(id = acs_colnames),
+                     data,
+                     by = "id",
+                     all.x = TRUE)
+
+    # Generate codes vector
+    codes <- matches$code
+    names(codes) <- matches$id
+
+    codes
+}
+
 # Convert an acs package dataset to a data frame
 convert_acs_data <- function(data) {
     assertthat::assert_that(acs::is.acs(data))
 
     # Convert ACS matrix to a data frame
     df <- data.frame(data@estimate, row.names = NULL)
+
+    # Rename columns based on coded variable names
+    coded_columns <- coded_variables(data@endyear, data@span, data@acs.colnames)
+    colnames(df) <- coded_columns
 
     # Add geocode and tract variables
     df$geo_id <- geo_ids(data@geography)
@@ -40,7 +74,7 @@ construct <- function(name, data) {
 
     # Load data
     constr$endyear <- data@endyear
-    constr$df <- convert_acs_data(data)
+    constr$data <- convert_acs_data(data)
 
     # Apply generic construction method
     constr <- measure(constr)
--- acsmsrs/R/msr_child_poverty.R
+++ acsmsrs/R/msr_child_poverty.R
@@ -11,29 +11,29 @@ measure.child_poverty <- function(constr) {
     # younger by adding the lt5 and 5 counts
 
     # Create variables of interest by summing across unused cross-tabs
-    data <- data.frame()
+    data <- data.frame(row.names = rownames(constr$data))
     for (age in c("ALt5", "A5", "A6to17")) {
         for (level in c("IncPrev12moLtPov", "IncPrev12moGePov")) {
             # Add across family types to get all youth in given poverty situation
             data[code(level, age)] <-
-                constr$df[, code(level, "FamMar", age)] +
-                constr$df[, code(level, "FamOth", "HhGmNWife", age)] +
-                constr$df[, code(level, "FamOth", "HhGfNHus", age)]
+                constr$data[, code(level, "FamMar", age)] +
+                constr$data[, code(level, "FamOth", "HhGmNWife", age)] +
+                constr$data[, code(level, "FamOth", "HhGfNHus", age)]
         }
 
         # Add across poverty types to get all youth with measured poverty status
-        data[code("IncPrev12Mo", age)] <-
-            constr$df[, code("IncPrev12moLtPov", age)] +
-            constr$df[, code("IncPrev12moGePov", age)]
+        data[code("IncPrev12mo", age)] <-
+            data[, code("IncPrev12moLtPov", age)] +
+            data[, code("IncPrev12moGePov", age)]
     }
 
     # Construct desired measures
-    out_data <- data.frame()
+    out_data <- data.frame(row.names = row.names(data))
     for (age in c("ALt5", "A5", "A6to17")) {
         out_data[code("nPov", age)] <- data[, code("IncPrev12moLtPov", age)]
         out_data[code("wPov", age)] <- data[, code("IncPrev12mo", age)]
         out_data[code("rPov", age)] <-
-            data[, code("nPov", age)] / data[, code("wPov", age)]
+            out_data[, code("nPov", age)] / out_data[, code("wPov", age)]
     }
 
     out_data
