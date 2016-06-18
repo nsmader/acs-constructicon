40383a54cf5d50f9d2fd5ccf8b050ae222562d97
Dylan Stark <dylan.stark@gmail.com>
2016-06-18 17:25:45 -0500

    Refactors child poverty measure.

--- acsmsrs/R/msr_child_poverty.R
+++ acsmsrs/R/msr_child_poverty.R
@@ -7,18 +7,26 @@ register.child_poverty <- function(obj) {
 measure.child_poverty <- function(constr) {
     message("Constructing ", constr$name, " measures")
 
+    new_data <- compute_measure(orig_data = constr$data)
+
+    constr$data <- cbind(constr$data, new_data)
+
+    constr
+}
+
+compute_measure <- function(orig_data) {
     # /!\ Consider creating additional age categories, like Age 5 and
     # younger by adding the lt5 and 5 counts
 
     # Create variables of interest by summing across unused cross-tabs
-    data <- data.frame(row.names = rownames(constr$data))
+    data <- data.frame(row.names = rownames(orig_data))
     for (age in c("ALt5", "A5", "A6to17")) {
         for (level in c("IncPrev12moLtPov", "IncPrev12moGePov")) {
             # Add across family types to get all youth in given poverty situation
             data[code(level, age)] <-
-                constr$data[, code(level, "FamMar", age)] +
-                constr$data[, code(level, "FamOth", "HhGmNWife", age)] +
-                constr$data[, code(level, "FamOth", "HhGfNHus", age)]
+                orig_data[, code(level, "FamMar", age)] +
+                orig_data[, code(level, "FamOth", "HhGmNWife", age)] +
+                orig_data[, code(level, "FamOth", "HhGfNHus", age)]
         }
 
         # Add across poverty types to get all youth with measured poverty status
@@ -36,8 +44,5 @@ measure.child_poverty <- function(constr) {
             out_data[, code("nPov", age)] / out_data[, code("wPov", age)]
     }
 
-    # Set new data for construction
-    constr$data <- out_data
-
-    constr
+    out_data
 }
