3dddd06bc40e74a4f5d36c0e8f3bad289e018e1c
Dylan Stark <dylan.stark@gmail.com>
2016-09-25 14:38:35 -0500

    Adds more tests.

--- pickacs/tests/testthat/test_select.R
+++ pickacs/tests/testthat/test_select.R
@@ -40,11 +40,80 @@ child_poverty <- function(data,
     as.character(df[, c("variable")])
 }
 
-
 test_that("defaults select total for universe", {
     result <- child_poverty(cp)
 
-    expect_is(result, 'character')
+    expect_is(result, "character")
     expect_equal(length(result), 1)
-    expect_equal(result[1], 'B17006_001')
+    expect_equal(result[1], "B17006_001")
+})
+
+test_that("first level selection works", {
+    result <- child_poverty(cp, income = "IncPrev12MoLtPov")
+
+    expect_is(result, "character")
+    expect_equal(length(result), 1)
+    expect_equal(result[1], "B17006_002")
+})
+
+test_that("second level, single branch works", {
+    result_FamMar <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "FamMar"
+    )
+
+    expect_is(result_FamMar, "character")
+    expect_equal(length(result_FamMar), 1)
+    expect_equal(result_FamMar[1], "B17006_003")
+
+    result_OthFam <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "OthFam"
+    )
+
+    expect_is(result_OthFam, "character")
+    expect_equal(length(result_OthFam), 1)
+    expect_equal(result_OthFam[1], "B17006_007")
+})
+
+test_that("third level, single branch works", {
+    result_ALt5 <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "FamMar",
+      age = "ALt5"
+    )
+
+    expect_is(result_ALt5, "character")
+    expect_equal(length(result_ALt5), 1)
+    expect_equal(result_ALt5[1], "B17006_004")
+
+    result_A5 <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "FamMar",
+      age = "A5"
+    )
+
+    expect_is(result_A5, "character")
+    expect_equal(length(result_A5), 1)
+    expect_equal(result_A5[1], "B17006_005")
+
+    result_A6to17 <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "FamMar",
+      age = "A6to17"
+    )
+
+    expect_is(result_A6to17, "character")
+    expect_equal(length(result_A6to17), 1)
+    expect_equal(result_A6to17[1], "B17006_006")
+
+    result_OthFam_HhGmNWife <- child_poverty(cp,
+      income = "IncPrev12MoLtPov",
+      family = "OthFam",
+      householder = "HhGmNWife"
+    )
+
+    expect_is(result_OthFam_HhGmNWife, "character")
+    expect_equal(length(result_OthFam_HhGmNWife), 1)
+    expect_equal(result_OthFam_HhGmNWife[1], "B17006_008")
 })
